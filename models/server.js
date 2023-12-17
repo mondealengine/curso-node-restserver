1a80e729ebf6becc56a99e9e80a20ebeff586a4d
Montserrat Sancho <montse@deal-engine.com>
2023-12-17 15:36:28 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = '/api/usuarios';
+
+    //Middlewares
+    this.middlewares();
+
+    //Rutas de mi aplicación
+    this.routes();
+  }
+
+  middlewares() {
+    //CORS
+    this.app.use(cors());
+
+    //lectura y parseo del body
+    this.app.use(express.json());
+
+    //directorio público
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+  }
+
+  listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
